9228ccfe2e3545b2d5f679da1e57650fbb66189a
Tinii1601 <22521471@gm.uit.edu.vn>
2024-11-07 00:17:18 +0700

    update

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -4,11 +4,39 @@ class MyComponent extends React.Component {
   /*
   
   */
+  state = {
+    name: "David",
+    age: 30,
+  };
+
+  handleOnChangName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleOnClick = () => {
+    alert("Hello, " + this.state.name);
+  };
+
   render() {
     return (
       <div>
-        <h1>My Component</h1>
-        <p>This is my component.</p>
+        <div className="first">
+          <h1>My Component</h1>
+          <p>This is my component.</p>
+        </div>
+        <div className="second">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangName(event)}
+          />
+          My name is {this.state.name}
+        </div>
+        <div className="third">
+          <button onClick={() => this.handleOnClick()}>Click me</button>
+        </div>
       </div>
     );
   }
