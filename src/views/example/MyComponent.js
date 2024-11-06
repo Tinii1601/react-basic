b22a4b4a83acdd00dd0430bbe2b99702073d2736
Tinii1601 <22521471@gm.uit.edu.vn>
2024-11-06 19:06:20 +0700

    update

---
+++ src/views/example/MyComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  /*
+  
+  */
+  render() {
+    return (
+      <div>
+        <h1>My Component</h1>
+        <p>This is my component.</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
